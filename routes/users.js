c0320a64630706f652dc3bb302adc988046d0408
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-07-02 02:45:36 +0700

    add USERS rest-api-crud

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const model = require('../models/index');
+var bcrypt = require('bcrypt');
+const { where } = require('sequelize');
+var salt = bcrypt.genSaltSync(10);
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
@@ -16,7 +19,7 @@ router.get('/', async function(req, res, next) {
     }
     else
     {
-        res.json({
+        res.status(400).json({
           'status':'DATA EMPTY',
           'messages':'Data is Empty',
           'data': {}
@@ -25,7 +28,7 @@ router.get('/', async function(req, res, next) {
   }
   catch (error) {
     console.log(error);
-    res.json({
+    res.status(500).json({
       'status': 'ERROR',
       'messages': error.messages,
       'data': {}
@@ -33,16 +36,95 @@ router.get('/', async function(req, res, next) {
   }
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
+  try {
+    const{
+      name,
+      email,
+      password,
+      roles
+    } = req.body
 
+    const users = await model.users.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt),
+      roles
+    });
+
+    if(users){
+      res.status(201).json({
+        'status' : 'OK',
+        'messages' : 'User berhasil ditambahkan',
+        'data': users
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      'status': 'Error',
+      'messages': error.messages,
+      'data':{}
+    })
+  }
 });
 
-router.patch('/:id', function(req, res, next) {
+router.put('/:id', async function(req, res, next) {
+  try{
+    const usersId = req.params.id;
+    const{
+      name,
+      email,
+      password,
+      roles
+    } = req.body
 
+    const users = await model.users.update({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt),
+      roles
+    },{
+      where:{
+        id: usersId
+      }
+    })
+
+    if(users){
+      res.status(200).json({
+        'status': 'OK',
+        'messages': 'User berhasil diupdate',
+        'data': users,
+      })
+    }
+  }catch(error){
+    res.status(400).json({
+      'status': 'ERROR',
+      'messages': error.message,
+      'data': {},
+    })
+  }
 });
 
-router.delete('/:id', function(req, res, next) {
-  
+router.delete('/:id',async function(req, res, next) {
+  try {
+    const usersId = req.params.id;
+    const users = await model.users.destroy({ where: {
+      id: usersId
+    }})
+    if (users) {
+      res.json({
+        'status': 'OK',
+        'messages': 'User berhasil dihapus',
+        'data': users,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {},
+    })
+  }
 });
 
 module.exports = router;
